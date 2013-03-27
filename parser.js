c3e5793036c79fed95810f6424e2f4a56653e942
Nicolas Raynaud <github@nraynaud.com>
2013-03-27 04:08:18 +0100

    added counterclockwise arcs, tunable arc segment count

--- parser.js
+++ parser.js
@@ -36,15 +36,16 @@ function move(parsedMove) {
     }
 }
 
-function parseArc(line) {
+function parseArc(line, clockwise) {
     var targetPos = $.extend(cloneObject(machinePos), detectAxisMove(line));
-
     var radius = parseFloat(RADIUS_DETECTOR.exec(line)[1]);
     // I can't do maths, found there : https://github.com/grbl/grbl/blob/master/gcode.c#L430
     var x = targetPos.x - machinePos.x;
     var y = targetPos.y - machinePos.y;
     var h_x2_div_d = 4 * radius * radius - x * x - y * y;
     h_x2_div_d = -Math.sqrt(h_x2_div_d) / Math.sqrt(x * x + y * y);
+    if (!clockwise)
+        h_x2_div_d = -h_x2_div_d;
 
     var toCenterX = 0.5 * (x - (y * h_x2_div_d));
     var toCenterY = 0.5 * (y + (x * h_x2_div_d));
@@ -56,11 +57,12 @@ function parseArc(line) {
     var angularDiff = Math.atan2(-toCenterX * targetCenterY + toCenterY * targetCenterX,
         -toCenterX * targetCenterX - toCenterY * targetCenterY);
     var angularStart = Math.atan2(-toCenterY, -toCenterX);
-    for (var i = 0; i <= 10; i++) {
-        var angle = angularStart + angularDiff * i / 10;
+    var arcSegments = 20;
+    for (var i = 0; i <= arcSegments; i++) {
+        var angle = angularStart + angularDiff * i / arcSegments;
         var px = centerX + radius * Math.cos(angle);
         var py = centerY + radius * Math.sin(angle);
-        move({x:px, y:py, z:machinePos.z});
+        move({x: px, y: py, z: ((machinePos.z * (arcSegments - i) + targetPos.z * i) / arcSegments)});
     }
 }
 
@@ -73,9 +75,11 @@ function evaluateCode() {
     path.push(machinePos);
     var text = $('#codebox').val();
     var arrayOfLines = text.match(/[^\r\n]+/g);
-    $.each(arrayOfLines, function (lineNo, line) {
+    $.each(arrayOfLines, function (lineNo, originalLine) {
         //drop spaces, go uppercase
-        line = line.replace(/[\t ]+/g, '').toUpperCase();
+        var line = originalLine.replace(/[\t ]+/g, '').toUpperCase();
+        // drop comments
+        line = line.replace(/[(][^)]*[)]/g, '');
         //drop line number
         line = line.replace(/^N[0-9]+/, '');
         var gCode = /^G([0-9.]+)(.*)/.exec(line);
@@ -83,14 +87,22 @@ function evaluateCode() {
             var codeNum = parseFloat(gCode[1]);
             if (codeNum == 0 || codeNum == 1)
                 move(detectAxisMove(gCode[2]));
-            else if (codeNum == 2 || codeNum == 3)
-                parseArc(gCode[2]);
+            else if (codeNum == 2)
+                parseArc(gCode[2], true);
+            else if (codeNum == 3)
+                parseArc(gCode[2], false);
+            else {
+                console.log('Did not understand the line, skipping');
+                console.log(originalLine);
+            }
         } else {
             var parsedMove = detectAxisMove(line);
             if (parsedMove)
                 move(parsedMove);
-            else
+            else {
                 console.log('Did not understand the line, skipping');
+                console.log(originalLine);
+            }
         }
 
     });
