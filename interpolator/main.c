42fb7d007a453440fed7625e534e900db9e57672
Nicolas Raynaud <github@nraynaud.com>
2013-05-04 15:07:04 +0200

    interpolator: wired all 3 axes

--- interpolator/main.c
+++ interpolator/main.c
@@ -7,6 +7,16 @@ extern void initUSB();
 
 void initUserButton();
 
+static const struct {
+    uint16_t xDirection, xStep, yDirection, yStep, zDirection, zStep;
+} pinout = {
+        .xDirection = GPIO_Pin_3,
+        .xStep = GPIO_Pin_4,
+        .yDirection = GPIO_Pin_5,
+        .yStep=GPIO_Pin_6,
+        .zDirection=GPIO_Pin_7,
+        .zStep=GPIO_Pin_8};
+
 int main(void) {
     //enable FPU
     SCB->CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2));
@@ -20,57 +30,58 @@ int main(void) {
     STM_EVAL_LEDOff(LED5);
     STM_EVAL_LEDOff(LED6);
 
-    GPIO_InitTypeDef GPIO_InitStructure;
-
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+    GPIO_InitTypeDef gpioConfig = {
+            .GPIO_Pin = pinout.xDirection | pinout.xStep | pinout.yDirection | pinout.yStep | pinout.zDirection | pinout.zStep,
+            .GPIO_Mode = GPIO_Mode_OUT,
+            .GPIO_Speed = GPIO_Speed_2MHz,
+            .GPIO_OType = GPIO_OType_PP,
+            .GPIO_PuPd = GPIO_PuPd_NOPULL};
+    GPIO_Init(GPIOE, &gpioConfig);
 
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-    NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
+    NVIC_InitTypeDef nvicConfig = {
+            .NVIC_IRQChannel = TIM3_IRQn,
+            .NVIC_IRQChannelPreemptionPriority = 0,
+            .NVIC_IRQChannelSubPriority = 0,
+            .NVIC_IRQChannelCmd = ENABLE};
+    NVIC_Init(&nvicConfig);
 
 
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-    TIM_OCInitTypeDef TIM_OCInitStructure;
     uint16_t PrescalerValue = (uint16_t) ((SystemCoreClock / 2) / clockFreq) - 1;
 
     TIM_Cmd(TIM3, DISABLE);
     TIM_UpdateRequestConfig(TIM3, TIM_UpdateSource_Regular);
     TIM_SelectOnePulseMode(TIM3, TIM_OPMode_Single);
     TIM_SetCounter(TIM3, 10000);
-    TIM_TimeBaseStructure.TIM_Period = 10000;
-    TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
-    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
-    TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+    TIM_TimeBaseInitTypeDef timeBaseConfig = {
+            .TIM_Period = 10000,
+            .TIM_Prescaler = PrescalerValue,
+            .TIM_ClockDivision = 0,
+            .TIM_CounterMode = TIM_CounterMode_Down};
+    TIM_TimeBaseInit(TIM3, &timeBaseConfig);
 
     /* Channel1 first for direction */
-    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-    TIM_OCInitStructure.TIM_Pulse = 2;
-    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-    TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+
+    TIM_OCInitTypeDef timeChannelConfig = {
+            .TIM_OCMode = TIM_OCMode_PWM1,
+            .TIM_OutputState = TIM_OutputState_Enable,
+            .TIM_Pulse = 2,
+            .TIM_OCPolarity = TIM_OCPolarity_High};
+    TIM_OC1Init(TIM3, &timeChannelConfig);
     TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Disable);
 
     /* Channel2 for step */
-    TIM_OCInitStructure.TIM_Pulse = 1;
-    TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+    timeChannelConfig.TIM_Pulse = 1;
+    TIM_OC2Init(TIM3, &timeChannelConfig);
 
     TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Disable);
     TIM_ITConfig(TIM3, TIM_IT_CC1| TIM_IT_CC2 | TIM_IT_Update, ENABLE);
 
     initUSB();
-
     initUserButton();
     while (1) {
     }
@@ -80,20 +91,18 @@ void initUserButton() {
     STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_GPIO);
     //init the interrupt by hand to set a lower-priority than what STM_EVAL_PBInit() would do
     SYSCFG_EXTILineConfig(USER_BUTTON_EXTI_PORT_SOURCE, USER_BUTTON_EXTI_PIN_SOURCE);
-    EXTI_InitTypeDef EXTI_InitStructure = {
+    EXTI_InitTypeDef extiConfig = {
             .EXTI_Line = USER_BUTTON_EXTI_LINE,
             .EXTI_Mode = EXTI_Mode_Interrupt,
             .EXTI_Trigger = EXTI_Trigger_Rising_Falling,
-            .EXTI_LineCmd = ENABLE
-    };
-    EXTI_Init(&EXTI_InitStructure);
-    NVIC_InitTypeDef NVIC_InitStructure = {
+            .EXTI_LineCmd = ENABLE};
+    EXTI_Init(&extiConfig);
+    NVIC_InitTypeDef nvicConfig = {
             .NVIC_IRQChannel = USER_BUTTON_EXTI_IRQn,
             .NVIC_IRQChannelPreemptionPriority = 0x0F,
             .NVIC_IRQChannelSubPriority = 0x0F,
-            .NVIC_IRQChannelCmd = ENABLE
-    };
-    NVIC_Init(&NVIC_InitStructure);
+            .NVIC_IRQChannelCmd = ENABLE};
+    NVIC_Init(&nvicConfig);
 }
 
 
@@ -114,8 +123,7 @@ typedef struct {
     axes_t axes;
 } step_t;
 
-
-step_t nextStep() {
+static step_t nextStep() {
     uint16_t nextDuration = 0;
     nextDuration |= readBuffer();
     nextDuration |= readBuffer() << 8;
@@ -130,9 +138,8 @@ step_t nextStep() {
 volatile uint8_t running = 0;
 volatile static step_t currentStep;
 
-void executeStep(step_t step) {
-    GPIO_ResetBits(GPIOE, GPIO_Pin_3);
-    GPIO_ResetBits(GPIOE, GPIO_Pin_4);
+static void executeStep(step_t step) {
+    GPIO_ResetBits(GPIOE, pinout.xDirection | pinout.xStep | pinout.yDirection | pinout.yStep | pinout.zDirection | pinout.zStep);
     currentStep = step;
     if (step.duration) {
         running = 1;
@@ -163,13 +170,25 @@ __attribute__ ((used)) void EXTI0_IRQHandler(void) {
 __attribute__ ((used)) void TIM3_IRQHandler(void) {
     if (TIM_GetITStatus(TIM3, TIM_IT_CC1) != RESET) {
         TIM_ClearITPendingBit(TIM3, TIM_IT_CC1);
+        uint16_t directions = 0;
         if (currentStep.axes.xDirection)
-            GPIO_SetBits(GPIOE, GPIO_Pin_3);
+            directions |= pinout.xDirection;
+        if (currentStep.axes.yDirection)
+            directions |= pinout.yDirection;
+        if (currentStep.axes.zDirection)
+            directions |= pinout.zDirection;
+        GPIO_SetBits(GPIOE, directions);
     }
     if (TIM_GetITStatus(TIM3, TIM_IT_CC2) != RESET) {
         TIM_ClearITPendingBit(TIM3, TIM_IT_CC2);
+        uint16_t steps = 0;
         if (currentStep.axes.xStep)
-            GPIO_SetBits(GPIOE, GPIO_Pin_4);
+            steps |= pinout.xStep;
+        if (currentStep.axes.yStep)
+            steps |= pinout.yStep;
+        if (currentStep.axes.zStep)
+            steps |= pinout.zStep;
+        GPIO_SetBits(GPIOE, steps);
     }
     if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) {
         TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
