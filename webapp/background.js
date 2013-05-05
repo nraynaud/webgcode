107d10fff11088a18620bba695c9a126de801691
Nicolas Raynaud <github@nraynaud.com>
2013-05-05 02:50:54 +0200

    renammed main webapp page

--- webapp/background.js
+++ webapp/background.js
@@ -1,9 +1,9 @@
 "use strict";
 chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('test.html', {
+    chrome.app.window.create('index.html', {
         bounds: {
-            width: 800,
-            height: 800
+            width: 900,
+            height: 900
         }
     });
 });
