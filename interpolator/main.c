665f0ccebcb239d5edb85575e1a32cc0e312802b
Nicolas Raynaud <github@nraynaud.com>
2013-09-12 13:48:39 +0200

    interpolator: fixed motor directions

--- interpolator/main.c
+++ interpolator/main.c
@@ -31,9 +31,9 @@ volatile cnc_memory_t cncMemory = {
 static const struct {
     unsigned int x:1, y:1, z:1;
 } motorDirection = {
-        .x = 1,
+        .x = 0,
         .y = 0,
-        .z = 1};
+        .z = 0};
 
 static step_t nextProgramStep() {
     uint16_t nextDuration = 0;
