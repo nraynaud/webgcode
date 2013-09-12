04bdc256430366a81da6f8a35671e7987629206e
Nicolas Raynaud <github@nraynaud.com>
2013-09-12 13:49:17 +0200

    box sample: made some surfacing example

--- samples/box.js
+++ samples/box.js
@@ -8,8 +8,8 @@ function createJoinTestSystem(machine) {
             return  l + x + ',' + y;
         }
 
-        var tailWidth = 4;
-        var tailHeight = direction * 7;
+        var tailWidth = 3;
+        var tailHeight = direction * 5;
         var tailSpread = 8;
 
         function sideWithJoin(len, dx, dy) {
@@ -47,11 +47,11 @@ function createJoinTestSystem(machine) {
 
     function createShape(joinOrientation, x, y) {
         var outline = machine.createOutline('M' + x + ',' + y + crateJoiningShape(45, 20, joinOrientation)).attr({id: 'outline'});
-        var rectangle = machine.filletWholePolygon(outline, 2);
+        var rectangle = machine.filletWholePolygon(outline, 1.5);
         outline.node.pathSegList.clear();
         rectangle[0].pushOnPath(outline);
         var rectangleToolPath = contouring(outline, toolRadius, false, false);
-        showClipperPolygon(paper, rectangleToolPath, 'blue');
+        showClipperPolygon(machine.paper, rectangleToolPath, 'blue');
         return rectangleToolPath;
     }
 
@@ -60,4 +60,53 @@ function createJoinTestSystem(machine) {
     var travelZ = 5;
     var workZ = -5;
     machine.setParams(workZ, travelZ, 600);
+}
+
+function drillBar(machine) {
+    var toolDiameter = 3;
+    var toolRadius = toolDiameter / 2;
+    var travelZ = 5;
+    var workZ = -5;
+
+    function peckDrill(x, y, z, topZ, steps) {
+        var polyline = new GeneralPolylineToolpath();
+        for (var i = 1; i <= steps; i++) {
+            polyline.pushPoint(x, y, topZ);
+            polyline.pushPoint(x, y, topZ - (topZ - z) * i / steps);
+        }
+        return polyline;
+    }
+
+    machine.setParams(workZ, travelZ, 3000);
+
+    for (var i = 1; i < 2; i++) {
+        machine.registerToolPath(peckDrill(0, -i * 4, -43, 0, 3));
+    }
+}
+
+function surfaceBar(machine) {
+    var toolDiameter = 7;
+    var toolRadius = toolDiameter / 2;
+    var travelZ = 6;
+    var workZ = -1;
+
+    machine.setParams(workZ, travelZ, 3000);
+
+    var width = 53;
+    var height = 100;
+    var stepOver = 0.2;
+
+    var toolPath = [];
+    var absoluteStepOver = toolDiameter * stepOver;
+    var startY = toolRadius - absoluteStepOver;
+    var stopY = height - toolRadius + absoluteStepOver;
+    for (var x = toolRadius - absoluteStepOver; x <= width - toolRadius + absoluteStepOver; x += toolDiameter * (1 - stepOver)) {
+        var line = new GeneralPolylineToolpath();
+        line.pushPoint(x, startY, workZ);
+        line.pushPoint(x, stopY, workZ);
+        toolPath.push(line);
+    }
+    console.log(toolPath);
+    machine.registerToolPathArray(toolPath);
+
 }
