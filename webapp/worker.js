0d08a744289ec22c277ccb7d52f68b1ec589dddc
Nicolas Raynaud <github@nraynaud.com>
2014-04-03 04:57:56 +0200

    cleanup in milling machine runner worker

--- webapp/worker.js
+++ webapp/worker.js
@@ -1,72 +1,27 @@
 "use strict";
-var $ = {each: function (array, func) {
-    for (var i = 0; i < array.length; i++)
-        func(i, array[i]);
-}, extend: function () {
-    /** stolen from JQUERY **/
-    var src, copyIsArray, copy, name, options, clone,
-        target = arguments[0] || {},
-        i = 1,
-        length = arguments.length,
-        deep = false;
-
-    // Handle a deep copy situation
-    if (typeof target === "boolean") {
-        deep = target;
-        target = arguments[1] || {};
-        // skip the boolean and the target
-        i = 2;
-    }
-
-    // Handle case when target is a string or something (possible in deep copy)
-    if (typeof target !== "object" && !jQuery.isFunction(target)) {
-        target = {};
-    }
-
-    // extend jQuery itself if only one argument is passed
-    if (length === i) {
-        target = this;
-        --i;
-    }
-
-    for (; i < length; i++) {
-        // Only deal with non-null/undefined values
-        if ((options = arguments[ i ]) != null) {
-            // Extend the base object
-            for (name in options) {
-                src = target[ name ];
-                copy = options[ name ];
-
-                // Prevent never-ending loop
-                if (target === copy) {
-                    continue;
+var $ = {
+    each: function (array, func) {
+        for (var i = 0; i < array.length; i++)
+            func(i, array[i]);
+    },
+    extend: function () {
+        /** stolen from JQUERY **/
+        var src, copy, name, options,
+            target = arguments[0] || {},
+            length = arguments.length;
+        for (var i = 0; i < length; i++)
+            if ((options = arguments[ i ]) != null)
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+                    if (target === copy)
+                        continue;
+                    if (copy !== undefined)
+                        target[name] = copy;
                 }
-
-                // Recurse if we're merging plain objects or arrays
-                if (deep && copy && ( jQuery.isPlainObject(copy) || (copyIsArray = jQuery.isArray(copy)) )) {
-                    if (copyIsArray) {
-                        copyIsArray = false;
-                        clone = src && jQuery.isArray(src) ? src : [];
-
-                    } else {
-                        clone = src && jQuery.isPlainObject(src) ? src : {};
-                    }
-
-                    // Never move original objects, clone them
-                    target[ name ] = jQuery.extend(deep, clone, copy);
-
-                    // Don't bring in undefined values
-                } else if (copy !== undefined) {
-                    target[ name ] = copy;
-                }
-            }
-        }
+        return target;
+        /** END stolen from JQUERY **/
     }
-
-    // Return the modified object
-    return target;
-    /** END stolen from JQUERY **/
-}
 };
 
 importScripts('libs/require.js');
@@ -112,7 +67,6 @@ require(['cnc/parser', 'cnc/simulation'], function (parser, simulation) {
             handleFragment(program);
             self.postMessage(null);
         };
-
     }
 
     self.onmessage = handleMessage;
