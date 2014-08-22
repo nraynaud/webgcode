53a5aa41e67b0cafdc6857e34153f06ba1354b96
Nicolas Raynaud <github@nraynaud.com>
2014-08-22 19:49:28 +0200

    renamed cam template to camApp

--- webapp/templates.js
+++ webapp/templates.js
@@ -1,4 +1,4 @@
-Ember.TEMPLATES["cam"] = Ember.Handlebars.template(function anonymous(Handlebars, depth0, helpers, partials, data) {
+Ember.TEMPLATES["camApp"] = Ember.Handlebars.template(function anonymous(Handlebars, depth0, helpers, partials, data) {
     this.compilerInfo = [4, '>= 1.0.0'];
     helpers = this.merge(helpers, Ember.Handlebars.helpers);
     data = data || {};
