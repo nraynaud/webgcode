13900c0ad65d5ecaf0693eab8f63b7ffe5221ba5
Nicolas Raynaud <github@nraynaud.com>
2014-09-26 17:20:32 +0200

    remove irrelevant comment in simplify.js

--- webapp/libs/simplify.js
+++ webapp/libs/simplify.js
@@ -8,7 +8,7 @@
     "use strict";
 
     // to suit your point format, run search/replace for '.x' and '.y';
-    // for 3D version, see 3d branch (configurability would draw significant performance overhead)
+    // for 3D version, see 3d branch
 
     function getX(p) {
         return p.X;
