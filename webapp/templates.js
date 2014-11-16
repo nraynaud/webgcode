ec56d643deb4abc7dd2c51f6e0216357084d220d
Nicolas Raynaud <github@nraynaud.com>
2014-11-16 23:53:48 +0100

    visucam: added shape template

--- webapp/templates.js
+++ webapp/templates.js
@@ -642,6 +642,16 @@ Ember.TEMPLATES["rampingContour"] = Ember.Handlebars.template(function anonymous
     return buffer;
 
 });
+Ember.TEMPLATES["shape"] = Ember.Handlebars.template(function anonymous(Handlebars, depth0, helpers, partials, data) {
+    this.compilerInfo = [4, '>= 1.0.0'];
+    helpers = this.merge(helpers, Ember.Handlebars.helpers);
+    data = data || {};
+    var buffer = '';
+
+
+    return buffer;
+
+});
 Ember.TEMPLATES["simpleContour"] = Ember.Handlebars.template(function anonymous(Handlebars, depth0, helpers, partials, data) {
     this.compilerInfo = [4, '>= 1.0.0'];
     helpers = this.merge(helpers, Ember.Handlebars.helpers);
