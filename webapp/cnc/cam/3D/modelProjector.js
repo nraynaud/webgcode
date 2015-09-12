e073cc93ebec4cd3fe6e6b49fd41547693758479
Nicolas Raynaud <github@nraynaud.com>
2015-09-12 07:16:59 +0200

    3D milling : integrate conservative rendering.

--- webapp/cnc/cam/3D/modelProjector.js
+++ webapp/cnc/cam/3D/modelProjector.js
@@ -6,7 +6,71 @@ define([], function () {
         this.scene = scene;
         this.angle = 0;
 
-        var fragmentShader = [
+        this.vertexShader = [
+            'attribute vec3 prevPoint;',
+            'attribute vec3 nextPoint;',
+            'uniform vec2 hPixel;',
+            'uniform vec2 hPixelWorld;',
+            'varying vec3 AABB_min;',
+            'varying vec3 AABB_max;',
+            'varying vec3 positionK;',
+
+            'float cross2d(vec2 v1, vec2 v2) {',
+            '    return v1.x * v2.y - v1.y * v2.x; ',
+            '}',
+
+            // http://http.developer.nvidia.com/GPUGems2/gpugems2_chapter42.html
+            'void main() {',
+            '    vec3 eyeDirection = vec3(0.0, 0.0, -1.0);',
+            '    vec3 p1 = prevPoint, p2 = position, p3 = nextPoint;',
+            '    vec2 e1 = normalize(p2.xy - p1.xy);',
+            '    vec2 e2 = normalize(p2.xy - p3.xy);',
+            // project the side on the bisector
+            // http://stackoverflow.com/a/32515402/72637
+            '    float halfsine = sqrt((1.0 - dot(e1, e2)) / 2.0);',
+            '    vec2 resultPoint2D = p2.xy + length(hPixelWorld) / halfsine * normalize(e1 + e2);',
+
+            // project the 2D point to the triangle plane in 3D
+            // grab the triangle normal
+            '    vec3 normal = normalize(cross(p2.xyz - p1.xyz, p3.xyz - p2.xyz));',
+            // grab the Z for (x=0, y=0)
+            '    float d = dot(normal, p2.xyz);',
+            // the new Z is the distance from the 2D projected point to its projection on the triangle plane
+            '    float t = (dot(normal, vec3(resultPoint2D, 0.0)) - d) / (dot(normal, eyeDirection));',
+
+            //shift the whole triangle up because Z is sampled at pixel center, but the maximum Z is at a corner.
+            //A mostly vertical triangle might send the Z very high or very low, we'll clamp that in the fragment shader
+            '    float cornerPessimization = sqrt(1.0 - normal.z * normal.z) *  length(hPixelWorld);',
+            '    vec4 shiftedPosition = vec4(resultPoint2D, t + cornerPessimization, 1.0);',
+            '    vec4 projectedShiftedPosition = projectionMatrix * modelViewMatrix * shiftedPosition;',
+
+            //compute the Axis Aligned bounding box
+            '    vec4 prevPos = projectionMatrix * modelViewMatrix * vec4(p1, 1.0);',
+            '    vec4 currPos = projectionMatrix * modelViewMatrix * vec4(p2, 1.0);',
+            '    vec4 nextPos = projectionMatrix * modelViewMatrix * vec4(p3, 1.0);',
+            '    vec3 minBounds = prevPos.xyz;',
+            '    minBounds = min(currPos.xyz, minBounds);',
+            '    minBounds = min(nextPos.xyz, minBounds);',
+            '    vec3 maxBounds = prevPos.xyz;',
+            '    maxBounds = max(currPos.xyz, maxBounds);',
+            '    maxBounds = max(nextPos.xyz, maxBounds);',
+            // extend the box by one pixel
+            '    minBounds = minBounds - vec3(hPixel, 0.0);',
+            '    maxBounds = maxBounds + vec3(hPixel, 0.0);',
+
+            '    AABB_min = minBounds;',
+            '    AABB_max = maxBounds;',
+            '    gl_PointSize = 10.0;',
+            '    positionK = projectedShiftedPosition.xyz;',
+            '    gl_Position = projectedShiftedPosition;',
+            '}'].join('\n');
+
+        this.fragmentShader = [
+            '#extension GL_EXT_frag_depth : enable',
+            'varying vec3 AABB_min;',
+            'varying vec3 AABB_max;',
+            'varying vec3 positionK;',
+            // depth encoding : http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
             'highp float factor = (exp2(24.0) - 1.0) / exp2(24.0);',
             'vec3 EncodeFloatRGB(highp float v) {',
             '   vec3 enc = fract(vec3(1.0, 255.0, 255.0 * 255.0) * factor * v);',
@@ -17,36 +81,32 @@ define([], function () {
             '   return dot(rgb, vec3(1.0, 1.0 / 255.0, 1.0 / 255.0 / 255.0)) / factor;',
             '}',
             'void main() {',
-            '   gl_FragData[0] = vec4(EncodeFloatRGB(1.0 - gl_FragCoord.z), 1.0);',
+            '    vec2 pos = positionK.xy;',
+            //let's destroy the fragments that are really out there between the input corner and the dilated corner
+            '    if( pos.x < AABB_min.x || pos.y < AABB_min.y ',
+            '            || pos.x > AABB_max.x || pos.y > AABB_max.y)',
+            '        discard;',
+            // ok, we were pessimistic, but one thing still holds:
+            // the true Z value can never ever be higher or lower than any Z value of the input vertices,
+            // so we clip to get back to some reality
+            '    float z = clamp(positionK.z, AABB_min.z, AABB_max.z);',
+            // go back to fragment world
+            '    z =  (0.5 * z + 0.5);',
+            // update the depth buffer, since what was a nice triangle is now a triangle with 2 bent corners (flattened by the Z clamp).
+            '    gl_FragDepthEXT = z;',
+            '    gl_FragData[0] = vec4(EncodeFloatRGB(1.0 - z), 1.0);',
             '}'].join('\n');
+        this.shaderAttributes = {prevPoint: {type: 'v3', value: null}, nextPoint: {type: 'v3', value: null}};
+        this.shaderUniforms = {hPixel: {type: 'v2'}, hPixelWorld: {type: 'v2'}};
         this.meshMaterial = new THREE.ShaderMaterial({
+            doublesided: true,
             depthTest: true,
             sizeAttenuation: false,
             linewidth: 1,
-            uniforms: {pixelDiagonal: {type: 'f'}},
-            vertexShader: [
-                'uniform float pixelDiagonal;',
-                'void main() {',
-                '   vec3 pos = position;',
-                '   float zError = sqrt(1.0 - normal.z * normal.z) * pixelDiagonal;',
-                '   pos.z = pos.z + zError;',
-                '   gl_PointSize = 1.0;',
-                '   gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);',
-                '}'].join('\n'),
-            // depth encoding : http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
-            fragmentShader: fragmentShader
-        });
-        this.linePointMaterial = new THREE.ShaderMaterial({
-            depthTest: true,
-            sizeAttenuation: false,
-            linewidth: 1,
-            vertexShader: [
-                'void main() {',
-                '   gl_PointSize = 1.0;',
-                '   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
-                '}'].join('\n'),
-            // depth encoding : http://aras-p.info/blog/2009/07/30/encoding-floats-to-rgba-the-final/
-            fragmentShader: fragmentShader
+            uniforms: this.shaderUniforms,
+            attributes: this.shaderAttributes,
+            vertexShader: this.vertexShader,
+            fragmentShader: this.fragmentShader
         });
         this.displaySide = 1024;
         this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1);
@@ -57,9 +117,11 @@ define([], function () {
 
     Projector.prototype = {
         render: function (renderer, buffer) {
+            renderer.getContext().getExtension('EXT_frag_depth');
+            this.meshMaterial.uniforms.hPixel.value = new THREE.Vector2(1 / buffer.width, 1 / buffer.height);
             var w = (this.camera.right - this.camera.left) / buffer.width;
             var h = (this.camera.top - this.camera.bottom) / buffer.height;
-            this.meshMaterial.uniforms.pixelDiagonal.value = Math.sqrt(w * w + h * h) / 2;
+            this.meshMaterial.uniforms.hPixelWorld.value = new THREE.Vector2(w / 2, h / 2);
             renderer.render(this.scene, this.camera, buffer, true);
         },
         setGeometry: function (meshGeometry) {
@@ -70,50 +132,25 @@ define([], function () {
             }
             this.model = new THREE.Object3D();
 
-            // we add the vertices to force webgl to raster on at least 1 pixel
-            // in case a "mountain" is spiky enough to slip thought the sampling grid.
-            var pointsGeom = new THREE.BufferGeometry();
-            pointsGeom.addAttribute('position', meshGeometry.attributes.position.clone());
+            var originalPosition = meshGeometry.attributes.position;
+            var attribute = originalPosition.clone();
+            var triangleCount = attribute.length / attribute.itemSize / 3;
 
-            // we should add all the edges too because a whole "ridge" might be sharp enough to slip through
-            // the raster grid
-            // I have a problem where the end of some lines "poke" slightly through the surface I don't know why.
-            // question here: http://stackoverflow.com/questions/27097236/strange-artefacts-in-depth-buffer-with-lines
-            // by orienting the lines towards -z, I don't get the poky things
-            var positions = meshGeometry.attributes.position.array;
-            var lines = [];
-            var vertical = new THREE.Vector3(0, 0, 1);
-
-            function push(x1, y1, z1, x2, y2, z2) {
-                var v = new THREE.Vector3(x2 - x1, y2 - y1, z2 - z1);
-                if (v.dot(vertical) / v.length() / vertical.length() > 0)
-                    lines.push(x1, y1, z1, x2, y2, z2);
-                else
-                    lines.push(x2, y2, z2, x1, y1, z1);
+            for (var triangleIndex = 0; triangleIndex < triangleCount; triangleIndex++) {
+                attribute.copyAt(triangleIndex * 3 + 0, originalPosition, triangleIndex * 3 + 2);
+                attribute.copyAt(triangleIndex * 3 + 1, originalPosition, triangleIndex * 3 + 0);
+                attribute.copyAt(triangleIndex * 3 + 2, originalPosition, triangleIndex * 3 + 1);
             }
-
-            for (var i = 0; i < positions.length; i += 9) {
-                var x1 = positions[i];
-                var y1 = positions[i + 1];
-                var z1 = positions[i + 2];
-
-                var x2 = positions[i + 3];
-                var y2 = positions[i + 4];
-                var z2 = positions[i + 5];
-
-                var x3 = positions[i + 6];
-                var y3 = positions[i + 7];
-                var z3 = positions[i + 8];
-
-                push(x1, y1, z1, x2, y2, z2);
-                push(x2, y2, z2, x3, y3, z3);
-                push(x3, y3, z3, x1, y1, z1);
+            meshGeometry.addAttribute('prevPoint', attribute);
+            attribute = originalPosition.clone();
+            for (triangleIndex = 0; triangleIndex < triangleCount; triangleIndex++) {
+                attribute.copyAt(triangleIndex * 3 + 0, originalPosition, triangleIndex * 3 + 1);
+                attribute.copyAt(triangleIndex * 3 + 1, originalPosition, triangleIndex * 3 + 2);
+                attribute.copyAt(triangleIndex * 3 + 2, originalPosition, triangleIndex * 3 + 0);
             }
+            meshGeometry.addAttribute('nextPoint', attribute);
+            meshGeometry.needsUpdate = true;
 
-            var linesGeometry = new THREE.BufferGeometry();
-            linesGeometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array(lines), 3));
-            this.model.add(new THREE.Line(linesGeometry, this.linePointMaterial, THREE.LinePieces));
-            this.model.add(new THREE.PointCloud(pointsGeom, this.linePointMaterial));
             var mesh = new THREE.Mesh(meshGeometry, this.meshMaterial);
             this.model.add(mesh);
             this.modelBbox = this.computeCameraBoundingBox();
@@ -124,9 +161,7 @@ define([], function () {
         resetCamera: function () {
             var bbox = this.modelBbox;
             var bboxSize = bbox.size();
-            var modelRatio = bboxSize.y / bboxSize.x;
             this.aspectRatio = bboxSize.x / bboxSize.y;
-            this.displaySideMm = (bboxSize.x > bboxSize.y ? bboxSize.x : bboxSize.y) * 1.1;
             this.camera.lookAt(new THREE.Vector3(0, 0, bbox.min.z));
             this.camera.near = 1;
             this.camera.position.set(0, 0, bbox.max.z + 10 + this.camera.near);
